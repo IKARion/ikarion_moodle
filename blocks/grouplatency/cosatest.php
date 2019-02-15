299081ec2999d31d33241d82bab5bf8e4bb6d2d5
Eugen Ebel <eugen.ebel@th-luebeck.de>
2019-02-15 13:07:31 +0100

    javascript lazyload

--- blocks/grouplatency/cosatest.php
+++ blocks/grouplatency/cosatest.php
@@ -4,7 +4,24 @@ require_once('../../config.php');
 
 global $CFG;
 
-require_once($CFG->dirroot . '/blocks/groupactivity/locallib.php');
+
+$req = [
+    'session' => '0',
+    'query' => 'contents_group',
+    'course' => 11,
+    'id' => 255
+];
+
+
+
+/*
+$req = [
+    'session' => '0',
+    'query' => 'contents_member',
+    'course' => 11,
+    //'id' => test_anonymize(236)
+    'id' => 'ouFVSEQq09GEg6Z/713Cdw=='
+];*/
 
 /*
 $req = [
@@ -23,9 +40,79 @@ $req = [
     //'id' => groupactivity\anonymize('304')
     'id' => 237
 ];
+*/
 
-$response = groupactivity\curl_request($req);
-$data = groupactivity\serialize($response);
+$response = test_curl_request($req);
+$data = test_serialize($response);
 
 print_object($data);
-*/
+
+
+
+
+function test_curl_request($data) {
+    $data_string = json_encode($data);
+
+    $url = \get_config('grouplatency', 'curl_url');
+    $port = \get_config('grouplatency', 'curl_port');
+    $token = \get_config('grouplatency', 'secrettoken');
+
+    $options = array(
+        CURLOPT_URL => $url,
+        CURLOPT_PORT => $port,
+        CURLOPT_RETURNTRANSFER => true,     // return web page
+        CURLOPT_HEADER => false,     //return headers in addition to content
+        CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+        CURLOPT_ENCODING => "",       // handle all encodings
+        CURLOPT_AUTOREFERER => true,     // set referer on redirect
+        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+        CURLOPT_TIMEOUT => 120,      // timeout on response
+        CURLOPT_MAXREDIRS => 10,       // stop after 10 redirects
+        CURLINFO_HEADER_OUT => true,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_SSL_VERIFYPEER => false,     // Disabled SSL Cert checks
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => $data_string,
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Token ' . $token,
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string),
+        )
+    );
+
+    $ch = curl_init();
+    curl_setopt_array($ch, $options);
+    $rough_content = curl_exec($ch);
+    $err = curl_errno($ch);
+    $errmsg = curl_error($ch);
+    $header = curl_getinfo($ch);
+    curl_close($ch);
+
+    return array(
+        "header" => $header,
+        "content" => $rough_content,
+    );
+}
+
+function test_serialize($response) {
+    $data = explode(PHP_EOL, $response['content']);
+
+    unset($data[0]);
+
+    foreach ($data as $key => $item) {
+        if (empty($item)) {
+            unset($data[$key]);
+        } else {
+            $data[$key] = json_decode($item);
+        }
+    }
+
+    return $data;
+}
+
+function test_anonymize($data) {
+    global $CFG;
+
+    return openssl_encrypt($data, $CFG->encryptmethod, $CFG->encryptkey);
+}
